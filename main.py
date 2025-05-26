008a7d162e5a73f3f040c237443e00448a79f8b5
JustSnoww <98555747+JustSnoww@users.noreply.github.com>
2025-05-26 14:11:43 -0500

    changed the hard coded path from frankenstein.txt to sys.argv builtin so you can analyze other documents based on commandline arugments

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+import sys
 from stats import get_word_count
 from stats import get_character_count
 from stats import sort_dict
 
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]
     text = get_book_text(path)
     num_words = get_word_count(text)
     char_dict = get_character_count(text)
     sort = sort_dict(char_dict)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count -----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count ---------")
